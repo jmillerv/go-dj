01a4c7dcd5d3fa7fae347e40eddd81ee87ab1b63
Jeremiah <jmiller.v@gmail.com>
2021-11-06 23:21:08 -0600

    feat(folder): implement Get(), getLocalFile() and Play()

--- content/folder.go
+++ content/folder.go
@@ -1,23 +1,37 @@
 package content
 
-import "os"
+import (
+	log "github.com/sirupsen/logrus"
+	"io/fs"
+	"io/ioutil"
+)
 
 // The folder structure exists because I didn't want to load an entire folder's worth
 // of songs into memory like the LocalFile struct does.
 
+// Folder is a struct for parsing folders that implements the Media interface
 type Folder struct {
 	Name    string
-	Content []*os.DirEntry
+	Content *[]fs.FileInfo
 	Path    string
 }
 
 func (f *Folder) Get() {
-	// load the files into the Content
-	panic("implement me")
+	log.Infof("buffering files from %s", f.Path)
+	folder, err := ioutil.ReadDir(f.Path)
+	if err != nil {
+		log.WithError(err).Error("unable to read folder from path")
+	}
+	f.Content = &folder
 }
 
 func (f *Folder) Play() {
 	// loop through the folder and play each as a local file
+	for _, file := range *f.Content {
+		l := f.getLocalFile(file)
+		l.Play()
+	}
+
 	panic("implement me")
 }
 
@@ -25,6 +39,11 @@ func (f *Folder) Stop() {
 	panic("implement me")
 }
 
-func (f *Folder) getLocalFile() *LocalFile {
-	return nil
+func (f *Folder) getLocalFile(file fs.FileInfo) *LocalFile {
+	l := &LocalFile{
+		Name: file.Name(),
+		Path: f.Path + "/" + file.Name(),
+	}
+	l.Get()
+	return l
 }
