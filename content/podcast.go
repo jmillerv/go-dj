4bc5c02385d3148a8747fa9f45aa25eeeed12152
Jeremiah <jmiller.v@gmail.com>
2022-12-09 23:53:06 -0500

    4: implement podcast.Get()

--- content/podcast.go
+++ content/podcast.go
@@ -1,23 +1,81 @@
 package content
 
-import log "github.com/sirupsen/logrus"
+import (
+	"github.com/mmcdole/gofeed"
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
+	"os/exec"
+)
+
+const (
+	playOrderNewest PlayOrder = "newest"
+	playOrderOldest PlayOrder = "oldest"
+	playOrderRandom PlayOrder = "random"
+)
+
+var pods podcasts // holds the feed data for podcasts
+var podcastStream streamPlayer
 
 type Podcast struct {
-	Name    string
-	URL     string
-	Path    string
-	Content []byte
+	Name      string
+	URL       string
+	Player    streamPlayer
+	PlayOrder PlayOrder // options: newest, oldest, random
 }
 
+type PlayOrder string
+
+// Get parses a podcast feed and sets the most recent episode as the Podcast content.
 func (p *Podcast) Get() error {
-	panic("implement me")
+	var ep *episode
+	parser := gofeed.NewParser()
+	feed, err := parser.ParseURL(p.URL)
+	if err != nil {
+		return err
+	}
+	log.Infof("test %v", feed.Description)
+	// traverse links
+	for _, item := range feed.Items {
+		pods.Episodes = append(pods.Episodes, item)
+	}
+
+	switch p.PlayOrder {
+	case playOrderNewest:
+		ep = pods.getNewestEpisode()
+	case playOrderOldest:
+		ep = pods.getOldestEpisode()
+	case playOrderRandom:
+		ep = pods.getRandomEpisode()
+	}
+
+	// setup podcast stream
+	podcastStream.playerName = ep.EpExtension
+	podcastStream.url = ep.EpURL
+	podcastStream.command = exec.Command(podcastStream.playerName, "-quiet", "-playlist", podcastStream.url)
+	podcastStream.in, err = podcastStream.command.StdinPipe()
+	if err != nil {
+		return errors.Wrap(err, "error creating standard pipe in")
+	}
+	podcastStream.out, err = podcastStream.command.StdoutPipe()
+	if err != nil {
+		return errors.Wrap(err, "error creating standard pipe out")
+	}
+
+	podcastStream.isPlaying = false
+
+	p.Player = podcastStream
+
+	return nil
 }
 
+// Play sends the audio to the output. It caches a played episode in the cache ofr later checks.
 func (p *Podcast) Play() error {
+	// play file
+	// cache played episode
 	panic("implement me")
 }
 
 func (p *Podcast) Stop() error {
-	log.Infof("Stopping stream from %v ", p.Path)
+	log.Infof("Stopping stream from %v ", p.URL)
 	return nil
 }
--- content/podcast_episodes.go
+++ content/podcast_episodes.go
@@ -2,6 +2,8 @@ package content
 
 import (
 	"github.com/mmcdole/gofeed"
+	"math/rand"
+	"time"
 )
 
 type podcasts struct {
@@ -10,32 +12,53 @@ type podcasts struct {
 }
 
 func (p *podcasts) getNewestEpisode() *episode {
-	return &episode{}
+	var newestEpisode *episode
+	for _, ep := range p.Episodes {
+		var date *time.Time
+		// TODO if played, log that it's in the cache, and skip to the next episode
+		if ep.PublishedParsed.After(*date) {
+			date = ep.PublishedParsed
+			newestEpisode.Item = ep
+			newestEpisode.EpURL = ep.Enclosures[0].URL
+			newestEpisode.EpExtension = ep.Enclosures[0].Type
+		}
+	}
+	return newestEpisode
 }
 
 func (p *podcasts) getOldestEpisode() *episode {
-
-	// get most recent by data
-
-	// check most recent against cache
-	// if played, get next most recent
-	return &episode{
-		Item:        nil,
-		EpExtention: "",
-		EpURL:       "",
+	var oldestEpisode *episode
+	// TODO if played, log that it's in the cache, and skip to the next episode
+	for _, ep := range p.Episodes {
+		var date *time.Time
+		if ep.PublishedParsed.Before(*date) {
+			date = ep.PublishedParsed
+			oldestEpisode.Item = ep
+			oldestEpisode.EpURL = ep.Enclosures[0].URL
+			oldestEpisode.EpExtension = ep.Enclosures[0].Type
+		}
 	}
+
+	return oldestEpisode
 }
 
 func (p *podcasts) getRandomEpisode() *episode {
-	return &episode{}
+	var randomEpisode *episode
+	rand.Seed(time.Now().UnixNano())
+	item := p.Episodes[rand.Intn(len(p.Episodes))]
+	// TODO if played, log that it's in the cache, and skip to the next episode
+	randomEpisode.Item = item
+	randomEpisode.EpExtension = item.Enclosures[0].Type
+	randomEpisode.EpURL = item.Enclosures[0].URL
+	return randomEpisode
 }
 
-func (p *podcasts) checkIfPlayed() bool {
+func (p *podcasts) checkIfPlayed(guid string) bool {
 	return false
 }
 
 type episode struct {
 	Item        *gofeed.Item // Keep this to hold the additional data
-	EpExtention string
+	EpExtension string
 	EpURL       string
 }
