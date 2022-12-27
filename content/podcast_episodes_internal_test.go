dc734e3c984b7dd955e6fd8743ec38d7f688a57e
Jeremiah <jmiller.v@gmail.com>
2022-12-26 20:13:20 -0500

    16: stub out unit tests

---
+++ content/podcast_episodes_internal_test.go
@@ -0,0 +1,131 @@
+package content
+
+// file labeled _internal_test because none of these functions are public.
+
+import (
+	"github.com/mmcdole/gofeed"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func Test_contains(t *testing.T) {
+	type args struct {
+		guids       []string
+		episodeGuid string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "Success: Returns True",
+			args: args{},
+			want: true,
+		},
+		{
+			name: "Success: Returns False",
+			args: args{},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := contains(tt.args.guids, tt.args.episodeGuid)
+			assert.ObjectsAreEqual(tt.want, got)
+		})
+	}
+}
+
+func Test_podcastCacheData_fromCache(t *testing.T) {
+	type fields struct {
+		Guids     []string
+		TTY       string
+		CacheDate time.Time
+	}
+	type args struct {
+		cacheData any
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   *podcastCacheData
+	}{
+		{},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &podcastCacheData{
+				Guids:     tt.fields.Guids,
+				TTY:       tt.fields.TTY,
+				CacheDate: tt.fields.CacheDate,
+			}
+			assert.Equalf(t, tt.want, p.fromCache(tt.args.cacheData), "fromCache(%v)", tt.args.cacheData)
+		})
+	}
+}
+
+func Test_podcasts_getNewestEpisode(t *testing.T) {
+	type fields struct {
+		Episodes []*gofeed.Item
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   episode
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &podcasts{
+				Episodes: tt.fields.Episodes,
+			}
+			assert.Equalf(t, tt.want, p.getNewestEpisode(), "getNewestEpisode()")
+		})
+	}
+}
+
+func Test_podcasts_getOldestEpisode(t *testing.T) {
+	type fields struct {
+		Episodes []*gofeed.Item
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   episode
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &podcasts{
+				Episodes: tt.fields.Episodes,
+			}
+			assert.Equalf(t, tt.want, p.getOldestEpisode(), "getOldestEpisode()")
+		})
+	}
+}
+
+func Test_podcasts_getRandomEpisode(t *testing.T) {
+	type fields struct {
+		Episodes []*gofeed.Item
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   episode
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &podcasts{
+				Episodes: tt.fields.Episodes,
+			}
+			assert.Equalf(t, tt.want, p.getRandomEpisode(), "getRandomEpisode()")
+		})
+	}
+}
